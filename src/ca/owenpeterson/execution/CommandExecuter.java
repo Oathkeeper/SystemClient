95b3d368ff48c4d1a8a6585618f8afe8c2080a91
Owen Peterson <owen.a.peterson@gmail.com>
2014-10-23 19:27:08 -0500

    Added declaration for new list of strings.

--- src/ca/owenpeterson/execution/CommandExecuter.java
+++ src/ca/owenpeterson/execution/CommandExecuter.java
@@ -1,9 +1,15 @@
 package ca.owenpeterson.execution;
 
+import java.util.List;
+
+import ca.owenpeterson.builder.CommandBuilder;
+
 public class CommandExecuter {
 	public CommandExecuter(){};
 	
 	public String executeCommand(String command) {
+		CommandBuilder commandBuilder = new CommandBuilder();
+		List<String> commandFragments = commandBuilder.buildCommandArrayFromString(command);
 		return "";
 	}
 
