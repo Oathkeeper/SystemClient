160a0357ee90f5c71985f3c2240eaeaa08f7c233
opeterson <owen.a.peterson@gmail.com>
2015-06-17 19:05:58 -0500

    added test comment

--- src/ca/owenpeterson/utils/FormatUtils.java
+++ src/ca/owenpeterson/utils/FormatUtils.java
@@ -1,5 +1,10 @@
 package ca.owenpeterson.utils;
 
+/**
+ * Added this comment to test pushing to github through eclipse.
+ * @author owen
+ *
+ */
 public class FormatUtils {
 
 	public static String reduceWhitespace(String stringToReduce) {
